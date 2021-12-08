54c5788ec613ff905d8c6e799d7e9a1aa0e00cb7
Mike Pizzica <mike.pizzica@gmail.com>
2021-12-07 21:20:58 -0500

    add starter API routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
+  // router.get('/', async (req, res) => {
+  //   try {
+  //     const locationData = await Location.findAll();
+  //     res.status(200).json(locationData);
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // });
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  // try {
+  //   const locationData = await Location.findByPk(req.params.id, {
+  //     // JOIN with travellers, using the Trip through table
+  //     include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+  //   });
+
+  //   if (!locationData) {
+  //     res.status(404).json({ message: 'No location found with this id!' });
+  //     return;
+  //   }
+
+  //   res.status(200).json(locationData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 // create new product
@@ -91,6 +117,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+
+  // try {
+  //   const locationData = await Location.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   });
+
+  //   if (!locationData) {
+  //     res.status(404).json({ message: 'No location found with this id!' });
+  //     return;
+  //   }
+
+  //   res.status(200).json(locationData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 module.exports = router;
